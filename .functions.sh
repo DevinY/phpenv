0a216acfb1afb6a7f4b35aab45605f68719faed8
Devin Yang <devin@ccc.tc>
2022-10-21 15:28:12 +0800

    架構優化，加入可以自訂不同yml檔

--- .functions.sh
+++ .functions.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
+function default(){
+    DEFAULT=$(grep -Ei "^DEFAULT" .env|cut -d= -f2)
+    if [ -z $DEFAULT ];then
+        DEFAULT="docker-compose"
+    fi
+    echo $DEFAULT
+}
 
+function start {
+    mkdir -p etc/cache
+    touch authorized_keys
+    echo ${APP_URL}
+    PROJECT=$1
+    args=" -f $(default).yml "
+    for SERVICE in $(grep -Ei "^SERVICES" .env|cut -d= -f2|sed 's/"//g')
+    do
+        if [ -f "services/${SERVICE}.yml" ];then
+            args+=" -f services/${SERVICE}.yml "
+        fi
+    done
+    docker-compose -p ${PROJECT} ${args} up -d
+}
+
+function stop {
+    touch authorized_keys
+    echo ${APP_URL}
+    PROJECT=$1
+    args=" -f $(default).yml "
+    for SERVICE in $(grep -Ei "^SERVICES" .env|cut -d= -f2|sed 's/"//g')
+    do
+        if [ -f "services/${SERVICE}.yml" ];then
+            args+=" -f services/${SERVICE}.yml "
+        fi
+    done
+    docker-compose -p ${PROJECT} ${args} down --remove-orphans
+}
 function exec_bash {
     echo "The \"$(grep -E 'PROJECT=.+$' .env|cut -d= -f2)\" project of the php container."
     docker-compose  -p ${PROJECT} \
@@ -15,7 +50,7 @@ function console() {
         touch authorized_keys
         PROJECT=$(grep 'PROJECT' .env|cut -d= -f2)
         echo ${APP_URL}
-        args=" -f docker-compose.yml "
+        args=" -f $(default).yml "
         for SERVICE in $(grep -i "SERVICES" .env|cut -d= -f2|sed 's/"//g')
         do
             if [ -f "services/${SERVICE}.yml" ];then
@@ -28,83 +63,3 @@ function console() {
     fi
 }
 
-if [[ "$1" = 'info' && "$2" = 'all' ]];then
-    . ./all
-    exit
-fi
-
-if [[ "$1" = 'info' && "$2" = 'ports' ]];then
-    ./ports
-    exit
-fi
-
-if [[ "$1" = 'all' ]];then
-    . ./all
-    exit
-fi
-
-if [ "$1" = 'info' ];then
-    . ./info
-    exit
-fi
-
-if [ "$1" = 'stop' ];then
-    shift 1
-    . ./stop $2
-    exit
-fi
-
-if [ "$1" = 'start' ];then
-    shift 1
-    . ./start
-    exit
-fi
-
-if [ "$1" = 'link' ];then
-    shift 1
-    . ./link
-    exit
-fi
-#reload nginx
-if [ "$1" = 'reload' ];then
-    . ./reload
-    exit
-fi
-
-#執行容器內的命令
-if [ "$1" = 'artisan' ];then
-    docker-compose -p ${PROJECT} exec php php $@
-    exit 0
-fi
-
-#Default Workspace
-if [ "$1" = '' ];then
-    exec_bash php
-fi
-
-#進入ssh容器
-if [ "$1" = 'ssh' ];then
-    exec_bash $1
-fi
-
-#進入Drive容器
-if [ "$1" = 'drive' ];then
-    exec_bash $1 public
-fi
-
-#檢測是否為.env名稱，如果是就進入
-if [ "$1" ];then
-    MATCH=$(ls envs/$1* 2>/dev/null|wc -l)
-    if [ ${MATCH} -eq 1 ];then
-        ls envs/$1* >/dev/null
-        if [ $? -eq 0 ];then
-            ENVFILE=$(ls envs/$1*)
-            #重新連結並進入容器
-            ln -sf ${ENVFILE} .env
-            grep "PROJECT" .env
-            shift 1
-            console exec php bash
-        fi
-        exit
-    fi
-fi
